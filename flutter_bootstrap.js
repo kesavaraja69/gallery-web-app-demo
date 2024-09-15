b02c19e8267ab7b2588d3996bfc3afb41d4ae9b3
kesavaraja69 <108524776+kesavarajam@users.noreply.github.com>
2024-09-15 07:38:29 +0530

    Image animation is Added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1044025149"
+    serviceWorkerVersion: "1974326462"
   }
 });
